94da9e34c1171d9bcec00bf077525cd80033ba68
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-02-27 16:27:49 +0100

    second commit

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,23 @@
+package com.crud.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+
+		/*TaskDto taskDto = new TaskDto(
+				(long) 1,
+				"Test title",
+				"I want to be a coder!");
+
+		Long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+
+		System.out.println(id + " " + title + " " + content);*/
+		SpringApplication.run(TasksApplication.class, args);
+	}
+}
---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,9 @@
+package com.crud.tasks.domain;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+
+    //What to doooooo???!!!
+}
