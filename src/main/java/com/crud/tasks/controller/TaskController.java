793e5eeea80ba4d693c73e8f0c576eba100c1aa7
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-05-11 22:46:10 +0200

    Zmiany dla Heroku nowe

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/task")
 @RequiredArgsConstructor
