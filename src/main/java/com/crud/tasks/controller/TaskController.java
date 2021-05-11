4bea0f245a7402c3829cf6bf8fddd3732b9a575e
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-05-11 22:06:42 +0200

    CORS usunięte

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/task")
 @RequiredArgsConstructor
